74bc80867f0bfc3230a5623cdf203a06047dd9ab
Shinokodomo <pedobiric@gmail.com>
2024-06-25 19:22:38 +0300

    Delete Task_1.py

--- Task_1.py
+++
@@ -1,9 +0,0 @@
-import math
-n, m = map(int, input('Введите аргументы через пробел:').split())
-i = 1
-while True:
-    print(i, end='')
-    i = 1 + (i + m - 2) % n
-    if i == 1:
-        break
-print()
