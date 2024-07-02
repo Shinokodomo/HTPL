ef432c26d3b86451068d5c87d77ef531a5e62c17
Shinokodomo <pedobiric@gmail.com>
2024-07-03 00:21:59 +0300

    Update Task 4.py

--- Task 4/Task 4.py
+++ Task 4/Task 4.py
@@ -1,13 +1,19 @@
 import sys
-import numpy as np
 
 def min_moves_to_equal_elements(filename):
     # Чтение чисел из файла
     with open(filename, 'r') as file:
         nums = [int(line.strip()) for line in file.readlines()]
 
+    # Сортировка чисел
+    nums.sort()
+    
     # Нахождение медианы
-    median = int(np.median(nums))
+    n = len(nums)
+    if n % 2 == 1:
+        median = nums[n // 2]
+    else:
+        median = (nums[n // 2 - 1] + nums[n // 2]) // 2
     
     # Вычисление минимального количества ходов
     moves = sum(abs(num - median) for num in nums)
